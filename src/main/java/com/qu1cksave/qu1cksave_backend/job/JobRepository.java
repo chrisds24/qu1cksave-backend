6b557928a7543aeea6c3d9106c85bae3d45962bf
Christian Delos Santos <77574945+chrisds24@users.noreply.github.com>
2025-03-07 05:30:01 -0800

    need to work on native query to do join for getJobs

--- src/main/java/com/qu1cksave/qu1cksave_backend/job/JobRepository.java
+++ src/main/java/com/qu1cksave/qu1cksave_backend/job/JobRepository.java
@@ -9,4 +9,16 @@ import java.util.UUID;
 public interface JobRepository extends JpaRepository<Job, UUID> {
     List<Job> findByMemberId(UUID memberId);
     Optional<Job> findByIdAndMemberId(UUID id, UUID memberId);
+
+    // https://stackoverflow.com/questions/51626520/whether-using-queries-in-jpa-repositories-is-proper-practice
+    // - Has a @Query to do a JOIN
+    // https://stackoverflow.com/questions/42966967/creating-a-custom-query-with-spring-data-jpa
+    // - Has example of native query using @Query(nativeQuery=true)
+    // https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html
+    // - Has @Query info
+    // - @Query(nativeQuery=true)
+    // - @NativeQuery
+    // - Named parameters
+    // - Activating Hibernate comments
+    //   -- spring.jpa.properties.hibernate.use_sql_comments=true
 }
