a9815f9389d3486ac3fc5c69ede6094f068a2dd7
Christian Delos Santos <77574945+chrisds24@users.noreply.github.com>
2025-06-05 01:22:54 -0700

    working on tests

--- src/test/java/com/qu1cksave/qu1cksave_backend/Qu1cksaveBackendApplicationTests.java
+++ src/test/java/com/qu1cksave/qu1cksave_backend/Qu1cksaveBackendApplicationTests.java
@@ -41,35 +41,6 @@ class Qu1cksaveBackendApplicationTests {
 
 	private static String postgresPassword = System.getenv("POSTGRES_PASSWORD");
 
-	private static final String testJob = """
-			{
-				"title": "test swe",
-				"company_name": "test company",
-				"job_description": "test job description",
-				"notes": "test notes",
-				"is_remote": "Remote",
-				"salary_min": 75000,
-				"salary_max": 120000,
-				"country": "US",
-				"us_state": "CA",
-				"city": "San Diego",
-				"date_applied": {
-					"year": 2025,
-					"month": 4,
-					"date": 9
-				},
-				"date_posted": {
-					"year": 2025,
-					"month": 4,
-					"date": 8
-				},
-				"job_status": "Applied",
-				"links": ["https://www.linkedin.com/jobs/view/4125105888/?alternateChannel=search&refId=L%2FaJTJBeDgAXxHgZ%2B3%2FBAw%3D%3D&trackingId=GQdu9ntQnwrk1Hxp2qSNAQ%3D%3D", "https://jobs.ashbyhq.com/clinical-notes-ai/3d10314e-9af5-4ec3-8cb7-9edd8e32a3e9"],
-				"found_from": "LinkedIn"
-			}
-		""";
-
-
 	// This will use a random port
 	@Container
 	static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest")
@@ -102,6 +73,103 @@ class Qu1cksaveBackendApplicationTests {
 
 	@Autowired
 	private WebTestClient webTestClient;
+
+	// TODO: (6/4/25) Tests I need
+	//  - Get multiple jobs, userId query param is logged in user's id
+	//  - Get multiple jobs, userId query param is NOT logged in user's id
+	//  - ??? Get multiple jobs, no userId query param provided
+	//    -- Spring/Java should throw out an error for this. Wonder what it
+	//       exactly is?
+	//  --------------------------
+	//  - Get one job, job exists
+	//  - Get one job, job doesn't exist
+	//    -- Covers case when user specifies an id of a job that doesn't belong
+	//       to them (since single jobs are found by id and userId)
+	//  - ??? Get one job, no id provided
+	//  --------------------------
+	//  - Create job w/o files, then get that job
+	//  - Create job w/ files, then get that job
+	//  - Create invalid job, missing required fields
+	//  - Create invalid job, extra fields
+	//   * For the last 2, I could use some kind of API validation, filters,
+	//     or custom code. API validation would be ideal, but custom code could
+	//     be enough for now
+	//  ---------------------------
+	//  - Edit job w/o files, then get that job
+	//  - Edit job w/ resume and cover letter added, then get that job
+	//  - Edit job w/ resume and cover letter edited, then get that job
+	//  - Edit job w/ resume and cover letter deleted, then get that job
+	//  - Stale job tests
+	//    *** Note, a job where its resume id and cover letter id are the same
+	//        as the latest version is not considered stale
+	//    *** How does a job become stale? Let's say there's tab A and tab B,
+	//        where tab A contains the stale job. The job is then edited in tab
+	//        B so that at least one of the files' id's are now different from
+	//        what tab A has
+	//        * Note that the following also applies to cover letter
+	//        * MAKE TESTS FOR THESE 3
+	//    1.) Job has no resume, then edited to have a resume. Edit job again,
+	//       but using the old job w/o the resume
+	//    2.) Job has a resume, then edited to have no resume. Edit job again,
+	//       but using the old job w/ the resume
+	//    3.) Job has a resume, then edited to have a different resume id (by
+	//       removing the resume, then adding one again). Edit job again,
+	//       but using the old job w/ the old resume id
+	//    *** Other possible issues:
+	//    -- Same resume id. User then attempts to edit resume by editing the
+	//       stale job
+	//       + Resume gets updated to this new resume, which is appropriate
+	//         * Note that both the metadata and the file in S3 would get
+	//           updated accordingly. The job also gets updated using the
+	//           updates made to the stale job
+	//    -- Same resume id. User then attempts to remove resume by editing the
+	//       stale job.
+	//       + Resume gets removed from S3 and database, as expected
+	//    *** IMPORTANT: I went with just leaving it to the user to not do
+	//        their work in stale tabs, then just having checks where errors
+	//        could occur.
+	//        - An alternative would have been to have a timestamp for the
+	//          most recent update timestamp, and not allow edits/deletes if
+	//          using a stale job (aka an outdated update timestamp)
+	//  ---------------------------
+	//  - Delete job, job exists
+	//  - Delete job, job doesn't exist (Stale list)
+	//    -- Here, stale refers to the list not having up to date jobs
+	//  - Delete job, but job is stale (see description in edit job tests)
+	//    -- The job and the up to date files get deleted since the delete uses
+	//       the file ids from the job obtained from the database
+ 	//  ---------------------------
+	//  LATER: Not logged in tests for each endpoint
+	//  +++++++++++++++++++++++++++
+	//  The ??? in front means I'm not sure if I even need this test
+
+	private static final String testJob = """
+			{
+				"title": "test swe",
+				"company_name": "test company",
+				"job_description": "test job description",
+				"notes": "test notes",
+				"is_remote": "Remote",
+				"salary_min": 75000,
+				"salary_max": 120000,
+				"country": "US",
+				"us_state": "CA",
+				"city": "San Diego",
+				"date_applied": {
+					"year": 2025,
+					"month": 4,
+					"date": 9
+				},
+				"date_posted": {
+					"year": 2025,
+					"month": 4,
+					"date": 8
+				},
+				"job_status": "Applied",
+				"links": ["https://www.linkedin.com/jobs/view/4125105888/?alternateChannel=search&refId=L%2FaJTJBeDgAXxHgZ%2B3%2FBAw%3D%3D&trackingId=GQdu9ntQnwrk1Hxp2qSNAQ%3D%3D", "https://jobs.ashbyhq.com/clinical-notes-ai/3d10314e-9af5-4ec3-8cb7-9edd8e32a3e9"],
+				"found_from": "LinkedIn"
+			}
+		""";
 
 	@Test
 	void contextLoads() {
@@ -548,8 +616,11 @@ class Qu1cksaveBackendApplicationTests {
 // https://spring.io/guides/gs/testing-web
 // https://www.baeldung.com/spring-boot-testing
 // https://medium.com/@mbanaee61/api-testing-in-spring-boot-2a6d69e5c3ce
+//
+// Main sources used for actual tests:
 // https://rieckpil.de/spring-boot-testing-mockmvc-vs-webtestclient-vs-testresttemplate/
 // https://rieckpil.de/spring-webtestclient-for-efficient-testing-of-your-rest-api/
+//
 // https://www.baeldung.com/docker-test-containers
 // https://www.baeldung.com/spring-boot-testcontainers-integration-test
 // https://dev.to/mspilari/integration-tests-on-spring-boot-with-postgresql-and-testcontainers-4dpc
